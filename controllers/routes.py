a74f2a24030f03a2b9f09eb8c9ad831949012638
opelhoward <opelhoward@yahoo.com>
2016-05-27 20:30:10 +0700

    Fix routes blueprint

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,23 +1,23 @@
 from flask import Blueprint, render_template
 
-app = Blueprint("routes", __name__, url_prefix="/")
+app = Blueprint("routes", __name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/pasang-iklan')
+@app.route("/pasang-iklan")
 def add_ads():
-    return render_template('add-ads.html')
+    return render_template("add-ads.html")
 
 
-@app.route('/daftar-produk')
+@app.route("/daftar-produk")
 def list_products():
-    return render_template('product-list.html')
+    return render_template("product-list.html")
 
 
-@app.route('/product/<product_name>')
+@app.route("/product/<product_name>")
 def get_product_desc(product_name):
-    return render_template('product-desc.html')
+    return render_template("product-desc.html")
