3c308e8a67852f422310e8dc4a622c226d151cab
melvinfonda <fondamelvin@gmail.com>
2016-05-27 22:36:41 +0700

    "final layout done"

--- app.py
+++ app.py
@@ -5,20 +5,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', a=1)
+    return render_template('index.html')
 
 @app.route('/pasangIklan')
 def pasangIklan():
-    return render_template('pasangIklan.html', a=1)
+    return render_template('pasangIklan.html')
 
 @app.route('/produkList')
 def produkList():
-    return render_template('produkList.html', a=1)
+    return render_template('produkList.html')
 
 @app.route('/deskripsiProduk')
 def deskripsiProduk():
-    return render_template('deskripsiProduk.html', a=1)
+    return render_template('deskripsiProduk.html')
 
+@app.route('/deskripsiEcommerce')
+def deskripsiEcommerce():
+    return render_template('deskripsiEcommerce.html')
+
+@app.route('/adminPage')
+def adminPage():
+    return render_template('adminPage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
