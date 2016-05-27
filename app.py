7e14c8c098ad098eb78ef521731aa436af3b91e9
melvinfonda <fondamelvin@gmail.com>
2016-05-27 19:45:07 +0700

    "pasang iklan, deksprisi produk, listproduk jadi"

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html', a=1)
 
+@app.route('/pasangIklan')
+def pasangIklan():
+    return render_template('pasangIklan.html', a=1)
+
+@app.route('/produkList')
+def produkList():
+    return render_template('produkList.html', a=1)
+
+@app.route('/deskripsiProduk')
+def deskripsiProduk():
+    return render_template('deskripsiProduk.html', a=1)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
