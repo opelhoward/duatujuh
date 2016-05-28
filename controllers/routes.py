feb989a031707067327e68a6c15ed268206af6fe
opelhoward <opelhoward@yahoo.com>
2016-05-28 17:49:43 +0700

    Create product listing pagination and use database instead of mock up

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+import math
+from flask import Blueprint, render_template, request
+
+from models.productmodel import Product
 
 app = Blueprint("routes", __name__)
 
@@ -13,13 +16,34 @@ def add_ads():
     return render_template("add-ads.html")
 
 
-@app.route("/productList")
-def list_products():
-    return render_template("product-list.html")
+NUMBER_OF_ITEMS_IN_PAGE = 24
 
 
-@app.route("/product")
-def get_product_desc():
+@app.route("/category/<category_name>")
+def list_products(category_name):
+    page_number = request.args.get('page')
+    if page_number is None:
+        page_number = 1
+    products_pagination = Product.query \
+        .filter_by(category=" " + category_name) \
+        .paginate(page_number, NUMBER_OF_ITEMS_IN_PAGE)
+    products = products_pagination.items
+    number_of_products = products_pagination.total
+    number_of_pages = int(math.ceil(number_of_products / NUMBER_OF_ITEMS_IN_PAGE))
+    pagination_start = max(page_number - 3, 1)
+    pagination_end = min(number_of_pages, pagination_start + 6)
+    return render_template("product-list.html",
+                           category=category_name,
+                           products=products,
+                           number_of_pages=number_of_pages,
+                           curr_page_num=page_number,
+                           pagination_start=pagination_start,
+                           pagination_end=pagination_end
+                           )
+
+
+@app.route("/product/<product_id>")
+def get_product_desc(product_id):
     return render_template("product-desc.html")
 
 
