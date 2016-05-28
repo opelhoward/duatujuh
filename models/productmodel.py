5d0e3f73616a2610ed3699962f1a42d806e56e47
opelhoward <opelhoward@yahoo.com>
2016-05-28 13:22:23 +0700

    Update product model

--- models/productmodel.py
+++ models/productmodel.py
@@ -5,12 +5,12 @@ from models.basemodel import BaseModel
 class Product(BaseModel):
     __tablename__ = "products"
 
-    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     company = db.Column(db.String(50), nullable=True)
     category = db.Column(db.String(50), nullable=True)
     subcategory = db.Column(db.String(50), nullable=True)
-    product_name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Integer, nullable=True)
+    product_name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.String(50), nullable=True)
     image_link = db.Column(db.String(255), nullable=True)
     product_url = db.Column(db.String(255), nullable=True)
     description = db.Column(db.Text, nullable=True)
@@ -18,8 +18,7 @@ class Product(BaseModel):
     email = db.Column(db.String(50), nullable=True)
     phone_number = db.Column(db.String(50), nullable=True)
 
-    def __init__(self, id, company, category, subcategory, product_name, price, image_link, product_url, description):
-        self.id = id
+    def __init__(self, company, category, subcategory, product_name, price, image_link, product_url, description):
         self.company = company
         self.category = category
         self.subcategory = subcategory
