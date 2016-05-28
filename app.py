9d4a308bcafb39c8b1b97092cf3cadac756eee42
opelhoward <opelhoward@yahoo.com>
2016-05-28 17:48:48 +0700

    Add model creation

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import json
 from flask import Flask
 from flask.ext.script import Manager
 
+from classifier.product_classifier import ProductCategoryClassifier
+from classifier.product_database import CsvProductDbConnector
 from controllers import routes
 from models import db
 from models.productmodel import Product
@@ -23,7 +25,12 @@ def create_db():
 
 @manager.option("-p", "--path", help="JSON file path")
 @manager.option("-c", "--company", help="Company name")
-def insert_to_db(path, company):
+@manager.option("-mp", "--model-path", help="Model path", default=None)
+def insert_to_db(path, company, model_path):
+    classifier = None
+    if model_path is not None:
+        classifier = ProductCategoryClassifier()
+        classifier.load_model(model_path)
     with app.app_context():
         objects = list()
         print("Preparing to open %s" % path)
@@ -35,6 +42,10 @@ def insert_to_db(path, company):
                     continue
                 count += 1
                 print("Adding product %d out of %d" % (count, len(product_arr)))
+                if classifier is not None:
+                    (category, subcategory) = classifier.classify([(product_json['category'], product_json['subcategory'])])[0]
+                    product_json['category'] = category
+                    product_json['subcategory'] = subcategory
                 product = Product(company=company, **product_json)
                 objects.append(product)
                 if len(objects) is 100:
@@ -45,6 +56,17 @@ def insert_to_db(path, company):
             db.session.bulk_save_objects(objects)
             db.session.commit()
         print("Finish adding to database")
+
+
+@manager.option("-tp", "--training-path", help="Path to training data")
+@manager.option("-mp", "--model-path", help="Path to model")
+def create_learning_model(training_path, model_path):
+    print("Preparing to learn")
+    classifier = ProductCategoryClassifier()
+    db = CsvProductDbConnector(training_path)
+    classifier.build_model_from_data(db.sample_data(10000))
+    classifier.save_model(model_path)
+    print("Finish creating model")
 
 
 @manager.command
